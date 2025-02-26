c704308ebee330cbefe47e62df9960f0ee42ff11
hahuydz <haquanghuy2006abc@gmail.com>
2025-02-26 20:48:49 +0700

    b4

---
+++ b4.js
@@ -0,0 +1,5 @@
+let width = prompt("Nhập vào chiều rộng HCN");
+let height = prompt("Nhập vào chiều dài HCN ");
+let area = width * height;
+console.log(`Diện tích của hình chữ nhật là: ${area}`);
+document.write("Diện tích hình chữ nhật là: " + area);
