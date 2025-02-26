5cce53e3d2fde0e4dca5d1ae5ceaf431c82b668c
hahuydz <haquanghuy2006abc@gmail.com>
2025-02-26 20:49:31 +0700

    b7

---
+++ b7.js
@@ -0,0 +1,5 @@
+let Math = +prompt("Mời bạn nhập vào điểm môn thứ nhất: ");
+let physics = +prompt("Mời bạn nhập vào điểm môn học thứ hai: ");
+let chemistry = +prompt("Mời bạn nhập vào điểm môn học thứ ba: ");
+let average = (Math + physics + chemistry) / 3;
+document.write(`Điểm trung bình cộng của ba môn là: ${average}`);
