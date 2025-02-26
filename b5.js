4a01d5c7c3a9311f141ed2dac02e029eb18a14e5
hahuydz <haquanghuy2006abc@gmail.com>
2025-02-26 20:48:28 +0700

    b5

---
+++ b5.js
@@ -0,0 +1,4 @@
+let firstNumber = +prompt("Mời bạn nhập vào số thứ nhất: ");
+let secondNumber = +prompt("Mời bạn nhập vào số thứ hai: ");
+let total = firstNumber + secondNumber;
+    alert(`Tổng của 2 số là ${total}`);
